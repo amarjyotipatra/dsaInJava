c9677a507ac2c71606bad55e65262e62a91d522f
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2023-11-25 07:17:38 +0000

    multiplication

---
+++ fundamentals/multiplyNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      Scanner sc=new Scanner(System.in);
+      int num1=sc.nextInt();
+      int num2=sc.nextInt();
+      int num3=num1*num2;
+      System.out.print(num3);
+    };
+  };
