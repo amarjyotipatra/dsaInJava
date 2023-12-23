63810cf4f221bcf2d666a955b6c7a02ee53c5d67
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2023-12-23 05:38:26 +0000

    recursion

---
+++ Searching/numberOfOccurences.java
@@ -0,0 +1,20 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      Scanner sc=new Scanner(System.in);
+      int t=sc.nextInt();
+      while(t-->0){
+       int n=sc.nextInt();
+       int arr[]=new int[n];
+       int count=0;
+       for(int i=0;i<n;i++){
+         arr[i]=sc.next().charAt(0);
+         if(arr[i]=='x')
+           count++;
+       }
+       System.out.println(count);
+      }
+    }
+  }
