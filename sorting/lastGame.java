0ed35d776b91719fc6017e2d2b3f03fc8b7c893c
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2024-01-03 06:56:49 +0000

    sorting questions

---
+++ sorting/lastGame.java
@@ -0,0 +1,18 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      Scanner sc=new Scanner(System.in);
+      int t=sc.nextInt();
+      while(t-->0){
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+          arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(n%2!=0 ? arr[n/2] : arr[n/2-1]);
+      }
+    }
+  }
