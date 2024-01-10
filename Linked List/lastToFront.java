591759f13acd1698a1886364e9e7111190d94cc4
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2024-01-10 06:08:02 +0000

    linked list question

--- Linked List/lastToFront.java
+++ Linked List/lastToFront.java
@@ -77,3 +77,96 @@ while(temp!=null)
 System.out.println();
 
 }
+// Complete the lastElementFirst function below.
+
+
+
+/*
+
+For your reference:
+
+
+
+SinglyLinkedListNode {
+
+int data;
+
+SinglyLinkedListNode next;
+
+}
+
+To create a new node use below constructor
+
+public SinglyLinkedListNode(int nodeData)
+
+*/
+
+static SinglyLinkedListNode lastElementFirst(SinglyLinkedListNode head) {
+
+    if (head == null || head.next == null) {
+   
+           return head;
+   
+       }
+   
+       SinglyLinkedListNode current = head;
+   
+       SinglyLinkedListNode previous = null;
+   
+   
+   
+       // Traverse to the last element while keeping track of the previous node.
+   
+       while (current.next != null) {
+   
+           previous = current;
+   
+           current = current.next;
+   
+       }
+   
+   
+   
+       // Move the last element to the first.
+   
+       previous.next = null;
+   
+       current.next = head;     
+   
+       return current;
+   
+   }
+   
+   
+   
+   
+   private static final Scanner scanner = new Scanner(System.in);
+   
+   public static void main(String[] args) throws IOException {
+   
+   int testCases = scanner.nextInt();
+   
+   while (testCases-- > 0) {
+   
+   SinglyLinkedList llist = new SinglyLinkedList();
+   
+   int llistCount = scanner.nextInt();
+   
+   for (int i = 0; i < llistCount; i++) {
+   
+       int llistItem = scanner.nextInt();
+   
+       llist.insertNode(llistItem);
+   
+   }
+   
+   printLinkedList(lastElementFirst(llist.head));
+   
+   }
+   
+   scanner.close();
+   
+   }
+   
+   }
+   
