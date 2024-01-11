c55984f9348642c4b7e07a872844b783930d89e4
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2024-01-11 05:46:37 +0000

    linked list question

--- Linked List/InsertANode.java
+++ Linked List/InsertANode.java
@@ -77,3 +77,92 @@ System.out.println();
 }
 
 
+// Complete the insertSortedNode function below.
+
+
+
+/*
+
+For your reference:
+
+
+
+SinglyLinkedListNode {
+
+int data;
+
+SinglyLinkedListNode next;
+
+}
+
+To create a new node use below constructor
+
+public SinglyLinkedListNode(int nodeData)
+
+*/
+
+static SinglyLinkedListNode insertSortedNode(SinglyLinkedListNode head,int value) {
+
+    SinglyLinkedListNode newNode = new SinglyLinkedListNode(value);
+ 
+ 
+ 
+     if (head == null || value < head.data) {
+ 
+         newNode.next = head;
+ 
+         return newNode;
+ 
+     }
+ 
+     SinglyLinkedListNode temp=head;
+ 
+     while(temp!=null && temp.next!=null && temp.next.data<=value)
+ 
+       temp=temp.next;
+ 
+     newNode.next=temp.next;
+ 
+     temp.next=newNode;
+ 
+     
+ 
+     return head;
+ 
+ }
+ 
+ 
+ 
+ 
+ private static final Scanner scanner = new Scanner(System.in);
+ 
+ public static void main(String[] args) throws IOException {
+ 
+ int testCases = scanner.nextInt();
+ 
+ while (testCases-- > 0) {
+ 
+ SinglyLinkedList llist = new SinglyLinkedList();
+ 
+ int llistCount = scanner.nextInt();
+ 
+ for (int i = 0; i < llistCount; i++) {
+ 
+     int llistItem = scanner.nextInt();
+ 
+     llist.insertNode(llistItem);
+ 
+ }
+ 
+ int value= scanner.nextInt();
+ 
+ printLinkedList(insertSortedNode(llist.head,value));
+ 
+ }
+ 
+ scanner.close();
+ 
+ }
+ 
+ }
+
