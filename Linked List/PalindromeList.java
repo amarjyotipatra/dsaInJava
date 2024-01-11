90a6058124c8ef065408be3a871949905ea01083
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2024-01-11 05:51:01 +0000

    linked list question

--- Linked List/PalindromeList.java
+++ Linked List/PalindromeList.java
@@ -53,3 +53,131 @@ public void insertNode(int nodeData) {
 }
 
 }
+// Complete the palindromeLlist function below.
+
+/*
+
+For your reference:
+
+
+
+public SinglyLinkedListNode {
+
+int data;
+
+SinglyLinkedListNode next;
+
+}
+
+
+
+To create a new node use below constructor
+
+public SinglyLinkedListNode(int nodeData)
+
+*/
+
+
+
+static boolean palindromeLlist(SinglyLinkedListNode head) {
+
+    if (head == null)
+
+        return false;
+
+
+
+    SinglyLinkedListNode fast = head;
+
+    SinglyLinkedListNode slow = head;
+
+    Stack<Integer> stack = new Stack<>();
+
+
+
+    // Move fast to the end and slow to the middle of the list
+
+    while (fast != null && fast.next != null) {
+
+        stack.push(slow.data);
+
+        slow = slow.next;
+
+        fast = fast.next.next;
+
+    }
+
+
+
+    // If the list has odd number of elements, skip the middle element
+
+    if (fast != null) {
+
+        slow = slow.next;
+
+    }
+
+
+
+    // Compare the second half of the list with the elements in the stack
+
+    while (slow != null) {
+
+        if (slow.data != stack.pop()) {
+
+            return false; //Not a Palindrome
+
+        }
+
+        slow = slow.next;
+
+    }
+
+
+
+    return true; // It's a palindrome
+
+}
+
+
+
+
+
+
+private static final Scanner scanner = new Scanner(System.in);
+
+public static void main(String[] args) throws IOException {
+
+int testCases = scanner.nextInt();
+
+while (testCases-- > 0) {
+
+SinglyLinkedList llist = new SinglyLinkedList();
+
+int llistCount = scanner.nextInt();
+
+for (int i = 0; i < llistCount; i++) {
+
+    int llistItem = scanner.nextInt();
+
+    llist.insertNode(llistItem);
+
+}
+
+boolean res =palindromeLlist(llist.head);
+
+if(res)
+
+System.out.println("true");
+
+else
+
+    System.out.println("false");
+
+}
+
+scanner.close();
+
+}
+
+}
