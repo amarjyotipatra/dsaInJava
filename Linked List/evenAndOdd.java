ccff9614dad37f5c6c1fa1c66090bc27fde7dacb
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2024-01-15 05:16:19 +0000

    linked list question

---
+++ Linked List/evenAndOdd.java
@@ -0,0 +1,77 @@
+import java.util.*;
+
+class Main {
+
+static class SinglyLinkedListNode {
+
+public int data;
+
+public SinglyLinkedListNode next;
+
+public SinglyLinkedListNode(int nodeData) {
+
+    this.data = nodeData;
+
+    this.next = null;
+
+}
+
+}
+
+static class SinglyLinkedList {
+
+public SinglyLinkedListNode head;
+
+public SinglyLinkedListNode tail;
+
+public SinglyLinkedList() {
+
+    this.head = null;
+
+    this.tail = null;
+
+}
+
+public void insertNode(int nodeData) {
+
+    SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+
+
+    if (this.head == null) {
+
+        this.head = node;
+
+    } else {
+
+        this.tail.next = node;
+
+    }
+
+    this.tail = node;
+
+}
+
+}
+
+static void printLinkedList(SinglyLinkedListNode head)
+
+{
+
+SinglyLinkedListNode temp=head;
+
+while(temp!=null)
+
+{
+
+    System.out.print(temp.data+" ");
+
+    temp=temp.next;
+
+}
+
+System.out.println();
+
+}
+
+
